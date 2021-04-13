726896c6e7732f51cffd077e50d662a18eb1e85c
ylchong <cgynlai@gmail.com>
2021-04-13 23:54:12 +0800

    added searchStringArray method

--- javaproject1/src/Arraysearch/ArraySearch.java
+++ javaproject1/src/Arraysearch/ArraySearch.java
@@ -1,29 +1,27 @@
 package Arraysearch;
 
+
 public class ArraySearch {
 	
 
 	public static void main(String[] args) {
 		String[] concepts = {"abs", "poly", "encap", "inherit"};
+		String result = searchStringArray("poly", concepts);
+        System.out.println(result);
+		result = searchStringArray("encap1", concepts);
+		System.out.println(result);
+		
+	}
+	
+	public static String searchStringArray (String target, String[] arr) {
 		String result = "not found";
-		for (String concept : concepts) {
-			if (concept.equals("poly")) {
+		for (String element : arr) {
+			if(element.equals(target)) {
 				result = "found";
 				break;
 			}
 		}
-
-		System.out.println(result);
-		
-		result = "not found";
-		for (String concept : concepts) {
-			if(concept.equals("inherit")) {
-			  result = "found";
-			  break;
-			}
-		}
-		
-		System.out.println(result);
+		return result;
 	}
 
 }
