3d117e4de8a506d35565c9e3430e85d3370d44bd
ylchong <cgynlai@gmail.com>
2021-04-15 22:10:38 +0800

    add insect class

---
+++ javaproject1/src/Insect/Insect.java
@@ -0,0 +1,18 @@
+package Insect;
+
+public class Insect {
+	/* instance variable by convention declared below class.  This is class level declaration, 
+	 * same as method are called Class Member. 
+	 */
+	private double weight;
+	private int x;
+	private int y;
+	
+	// use main for test method 
+	public static void main(String[] args) {
+		Insect buzz1 = new Insect();
+		Insect buzz2 = new Insect();
+		Insect buzz3 = new Insect();
+	}
+
+}
