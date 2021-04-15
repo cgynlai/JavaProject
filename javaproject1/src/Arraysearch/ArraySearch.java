4696b40ffaddbfd459383e01b9c3b1748c3025b3
ylchong <cgynlai@gmail.com>
2021-04-15 12:14:20 +0800

    changed to boolean type method

--- javaproject1/src/Arraysearch/ArraySearch.java
+++ javaproject1/src/Arraysearch/ArraySearch.java
@@ -6,18 +6,26 @@ public class ArraySearch {
 
 	public static void main(String[] args) {
 		String[] concepts = {"abs", "poly", "encap", "inherit"};
-		String result = searchStringArray("poly", concepts);
-        System.out.println(result);
-		result = searchStringArray("encap1", concepts);
-		System.out.println(result);
+		String[] concepts2 = {"sunrise", "sunsets", "walks"};
+		
+		//String result = searchStringArray("poly", concepts);
+        //System.out.println(result);
+        //result = searchStringArray("encap", concepts);
+		//System.out.println(result);
+		
+		//shorten the code without 'result' variable, if 'result' will not be used anymore
+		System.out.println(searchStringArray("sunrise", concepts2));
+	
 		
 	}
 	
-	public static String searchStringArray (String target, String[] arr) {
-		String result = "not found";
+	//'public' is a visibility modifier, so that can call from outside the class
+	// 'static', no object or not going for instantiation
+	public static boolean searchStringArray (String target, String[] arr) {
+		boolean result = false;
 		for (String element : arr) {
 			if(element.equals(target)) {
-				result = "found";
+				result = true;
 				break;
 			}
 		}
